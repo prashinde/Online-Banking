2b1b56a489481dd7e569c543f535acfe8ab76863
Pratik Shinde <pratiks1@umbc.edu>
2017-10-05 05:52:23 -0400

    Plot graph

--- test/cl_test.sh
+++ test/cl_test.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 
-u="$1"
-rm -rf ../log/*
-for i in `seq 1 $u`
+ip=$1
+port=$2
+rate=$3
+
+GRAPH_DIR=graph
+for i in {1..3};
 do
-	cd ../bin/.
-	./client ../test/ctest 100 $2 $3 0 $i &
+	rm -rf ../log/*
+	for j in `seq 1 $i`
+	do
+		cd ../bin/.
+		./client ../test/ctest 100 $1 $2 $3 $j &
+	done
+
+	wait
+
+	cd ../log/.
+	mkdir -p ../$GRAPH_DIR
+	#rm -rf ../$GRAPH_DIR/*
+
+	avgtime=0.0
+	for file in ../log/*;
+	do 
+		line=$(tail -1 $file)
+		number=$(echo $line | cut -d':' -f2)
+		echo $number
+		avgtime=$(echo "scale=8;$avgtime+$number"|bc)
+	done
+
+	avgtime=$(echo "scale=8;$avgtime/$i"|bc)
+	printf "%d %08.8f\n" "$i" "$avgtime" >> ../$GRAPH_DIR/graph.plot
 done
